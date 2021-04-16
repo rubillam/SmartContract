1183a0dffd2f22b4391f7151de458f97737e27b9
rubillam <67844958+rubillam@users.noreply.github.com>
2021-04-16 15:51:32 -0400

    eliminacion de librerias no usadas

--- SmartContractAzurian/src/cl/az/sc/api/RequestApiKaleido.java
+++ SmartContractAzurian/src/cl/az/sc/api/RequestApiKaleido.java
@@ -1,17 +1,11 @@
 package cl.az.sc.api;
 //nnuevo comentario 
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
 
 public class RequestApiKaleido {
 	

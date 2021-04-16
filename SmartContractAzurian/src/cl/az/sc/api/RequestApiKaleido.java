3e64ccbaebc294299fad86a0de0db2eeba1063f2
rubillam <67844958+rubillam@users.noreply.github.com>
2021-04-16 15:53:11 -0400

    cambio librerias

--- SmartContractAzurian/src/cl/az/sc/api/RequestApiKaleido.java
+++ SmartContractAzurian/src/cl/az/sc/api/RequestApiKaleido.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 public class RequestApiKaleido {
 	
 	public HttpURLConnection getConexionEndpoint(String endpoint, String metodo, int conecttimeout, int readtimeout) throws IOException {
